c3b71f72db1faee014f1adde4a83d782322f04ad
Josh <joshua.a.beemster@gmail.com>
2015-06-30 11:00:32 +0200

    Bumped VERSION

--- src/main/scala/com.snowplowanalytics.snowplow.storage.kinesis/s3/SinkApp.scala
+++ src/main/scala/com.snowplowanalytics.snowplow.storage.kinesis/s3/SinkApp.scala
@@ -50,7 +50,7 @@ object SinkApp extends App {
   val parser = new ArgotParser(
     programName = "generated",
     compactUsage = true,
-    preUsage = Some("%s: Version %s. Copyright (c) 2013, %s.".format(
+    preUsage = Some("%s: Version %s. Copyright (c) 2015, %s.".format(
       generated.Settings.name,
       generated.Settings.version,
       generated.Settings.organization)
