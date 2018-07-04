7cf2988c56a087e3988a6470beaa499652c08f95
Arihant Surana <ari.surana@gmail.com>
2018-07-04 20:04:36 +1000

    Allow configurable and dynamic s3 path (closes #134)

--- src/main/scala/com.snowplowanalytics.s3/loader/S3Emitter.scala
+++ src/main/scala/com.snowplowanalytics.s3/loader/S3Emitter.scala
@@ -74,6 +74,7 @@ class S3Emitter(
 
   // create Amazon S3 Client
   private val bucket = config.bucket
+  private val directoryPattern = config.directoryPattern
   val log = LoggerFactory.getLogger(getClass)
   val client = AmazonS3ClientBuilder
     .standard()
@@ -153,6 +154,7 @@ class S3Emitter(
   def attemptEmit(namedStream: NamedStream, connectionAttemptStartTime: Long): Boolean = {
 
     var attemptCount: Long = 1
+    val connectionAttemptStartDateTime = new DateTime(connectionAttemptStartTime)
     while (true) {
       if (attemptCount > 1 && System.currentTimeMillis() - connectionAttemptStartTime > maxConnectionTime) {
         forceShutdown()
@@ -160,12 +162,14 @@ class S3Emitter(
 
       try {
         val outputStream = namedStream.stream
-        val filename = namedStream.filename
+
+        val s3Key = DynamicPath.decorateDirectoryWithTime(directoryPattern, namedStream.filename, connectionAttemptStartDateTime)
+
         val inputStream = new ByteArrayInputStream(outputStream.toByteArray)
 
         val objMeta = new ObjectMetadata()
         objMeta.setContentLength(outputStream.size.toLong)
-        client.putObject(bucket, filename, inputStream, objMeta)
+        client.putObject(bucket, s3Key, inputStream, objMeta)
 
         return true
       } catch {
