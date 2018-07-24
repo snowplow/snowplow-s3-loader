37dd19fdee979c3151c4d2aa04c0aabf0593abc1
arihantsurana <ari.surana@gmail.com>
2018-07-24 19:00:59 +1000

    Remove curly braces from transformed directory pattern

--- src/test/scala/com/snowplowanalytics/s3/loader/DynamicPathSpec.scala
+++ src/test/scala/com/snowplowanalytics/s3/loader/DynamicPathSpec.scala
@@ -25,25 +25,25 @@ class DynamicPathSpec extends Specification {
     "correctly decorate Time formats with one time pattern" in {
       val directoryPattern = Some("something/{YYYY}")
       val actual = DynamicPath.decorateDirectoryWithTime(directoryPattern, file, time)
-      actual must_== "something/{1970}/bar.gz"
+      actual must_== "something/1970/bar.gz"
     }
 
     "correctly decorate Time formats with multiple time patterns" in {
       val directoryPattern = Some("something/{YYYY}/{mm}dy={dd}")
       val actual = DynamicPath.decorateDirectoryWithTime(directoryPattern, file, time)
-      actual must_== "something/{1970}/{01}dy={01}/bar.gz"
+      actual must_== "something/1970/01dy=01/bar.gz"
     }
 
     "correctly decorate Time formats with extra trailing slash" in {
       val directoryPattern = Some("something/{YYYY}/{mm}dy={dd}/")
       val actual = DynamicPath.decorateDirectoryWithTime(directoryPattern, file, time)
-      actual must_== "something/{1970}/{01}dy={01}/bar.gz"
+      actual must_== "something/1970/01dy=01/bar.gz"
     }
 
     "correctly decorate Time formats with invalid time format" in {
       val directoryPattern = Some("something/{YYYY}/{mm}dy={dd}/{foo}")
       val actual = DynamicPath.decorateDirectoryWithTime(directoryPattern, file, time)
-      actual must_== "something/{1970}/{01}dy={01}/{foo}/bar.gz"
+      actual must_== "something/1970/01dy=01/foo/bar.gz"
     }
 
     "correctly handle no format" in {
