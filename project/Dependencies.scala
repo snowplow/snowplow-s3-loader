8b86f6032c039e7d3322364d3fbb74127892c233
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2020-09-24 12:19:56 +0200

    Update hadoop-common

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val slf4j            = "1.7.30"
     val kinesisClient    = "1.14.0"
     val kinesisConnector = "1.3.0"
-    val hadoop           = "2.7.3"
+    val hadoop           = "3.3.0"
     val elephantbird     = "4.17"
     val hadoopLZO        = "0.4.20"
     val jodaTime         = "2.10.6"
